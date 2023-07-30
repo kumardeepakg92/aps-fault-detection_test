400c1e8b077ad17b4f9466db03be02c71b17fe53
kumardeepakg92 <kumardeepakg92@gmail.com>
2023-07-30 11:31:23 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+def get_requirement():
+    pass
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Deepak Kumar",
+    author_email="kumardeepakg92@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirement()
+)
